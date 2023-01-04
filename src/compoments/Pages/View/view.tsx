3ca053303f33ba747e8d3ef3296cfb61406c4e79
chimnguu <57534799+chimnguu@users.noreply.github.com>
2023-01-05 03:12:59 +0900

    あああああ

--- src/compoments/Pages/View/view.tsx
+++ src/compoments/Pages/View/view.tsx
@@ -6,6 +6,7 @@ const loginCompoment: React.FC = () => (
         <Row>
             <Col span={24}>
                 <Divider orientation="left">Roblocc Panel - Blox Fruit</Divider>
+
             </Col>
         </Row>
 
