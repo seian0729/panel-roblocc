b0c1ea2fc31ba01ef82f41b129aa12cca15471e5
chimnguu <57534799+chimnguu@users.noreply.github.com>
2023-01-15 23:15:05 +0900

    + Add, Option for page size - tinh nang not vthang

--- src/compoments/Pages/View/view.tsx
+++ src/compoments/Pages/View/view.tsx
@@ -375,6 +375,15 @@ function DataCompoment()
                         rowKey={(record) => record.UsernameRoblocc}
                         pagination={{
                             total: dataApi.length,
+                            pageSizeOptions:[
+                                10,
+                                20,
+                                50,
+                                100,
+                                200,
+                                500,
+                                1000
+                            ],
                             showTotal: (total,range) => `${range[0]}-${range[1]} of ${total} items`,
                             current: page,
                             pageSize: pageSize,
