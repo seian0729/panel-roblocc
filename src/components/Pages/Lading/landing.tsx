5130dd9373fee9a2aa354ff948fa8842ba4ee08d
hoocdarealest <70364696+Hoocs151@users.noreply.github.com>
2023-06-30 15:31:59 +0700

    ùwú

--- src/components/Pages/Lading/landing.tsx
+++ src/components/Pages/Lading/landing.tsx
@@ -1,88 +1,74 @@
 import React from 'react';
-import {Button, Card, Col, Layout, Row, theme, Typography} from 'antd';
-import {Link} from 'react-router-dom';
+import { Button, Card, Col, Layout, Row, Typography } from 'antd';
+import { Link } from 'react-router-dom';
 
-import {useStore} from "../../../state/storeHooks";
+import { useStore } from '../../../state/storeHooks';
 
-import img from "../../../img/だいしきゅーだいしゅき_p1.png"
+import img from '../../../img/だいしきゅーだいしゅき_p1.png';
 
-const {Title, Paragraph} = Typography;
+const { Title, Paragraph } = Typography;
 
-const {Content, Footer} = Layout;
+const { Content, Footer } = Layout;
+
 const Landing: React.FC = () => {
-    const {user} = useStore(({app}) => app);
-    const userIsLogged = user.isSome();
-    const {} = theme.useToken();
+  const { user } = useStore(({ app }) => app);
+  const userIsLogged = user.isSome();
 
+  return (
+    <Layout style={{ background: 'linear-gradient(rgba(10, 10, 12, 0.9), rgba(10, 10, 12, 0.9)), url("https://cdn.discordapp.com/attachments/765845133308461077/1124247841305530448/p1.png") no-repeat center center fixed', WebkitBackgroundSize: 'cover', MozBackgroundSize: 'cover', OBackgroundSize: 'cover', backgroundSize: 'cover', animationName: 'cac', animationDuration: '10s', animationTimingFunction: 'linear', animationIterationCount: 'infinite' }}>
+      <Content>
+        <div id="banner" className="banner">
+          <Row justify="center" style={{ marginTop: 24 }} className="banner-title">
+            <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+              <Row>
+                <Col xs={24} sm={24} md={24} lg={24} xl={12} style={{ padding: 12, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <div>
+                    <Title level={3}>Just a Roblox Panel</Title>
+                    <Paragraph>Them bun long qua</Paragraph>
+                    {!userIsLogged ? (
+                      <Link to="login">
+                        <Button type="primary">Get Started</Button>
+                      </Link>
+                    ) : (
+                      <a href="dashboard">
+                        <Button type="primary">Dashboard</Button>
+                      </a>
+                    )}
+                  </div>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={24} xl={12} style={{ padding: 12 }}>
+                  <img alt="thumb" src={img} style={{ maxWidth: '100%', height: 'auto', borderRadius: 8 }} />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </div>
 
-    return (
-        <Content>
-            <div id={'banner'} className={'banner'}>
-                <Row justify={'center'} style={{marginTop: 24}} className={'banner-title'}>
-                    <Col xs={24} sm={24} md={24} lg={24} xl={12}>
-                        <Row>
-                            <Col xs={24} sm={24} md={24} lg={24} xl={12}
-                                 style={{padding: 12, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <div>
-                                    <Title level={3}>Just a Roblox Panel</Title>
-                                    <Paragraph>
-                                        Them bun long qua
-                                    </Paragraph>
-                                    {
-                                        !userIsLogged ?
-                                            <Link to={"login"}>
-                                                <Button type={"primary"}> {"Get Started"} </Button>
-                                            </Link>
-                                            :
-                                            <a href={"dashboard"}>
-                                                <Button type={"primary"}>Dashboard</Button>
-                                            </a>
-                                    }
-                                </div>
-                            </Col>
-                            <Col xs={24} sm={24} md={24} lg={24} xl={12} style={{padding: 12}}>
-                                <img alt={'thumb'} src={img}
-                                     style={{maxWidth: '100%', height: 'auto', borderRadius: 8}}/>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </div>
+        <div style={{ marginTop: 24, paddingBottom: 48, background: 'rgba(10, 10, 12, 0.2)', color: 'white' }}>
+          <div style={{ paddingTop: 12, textAlign: 'center' }}>
+            <Title level={2}>THEM BUN LONG QUA</Title>
+          </div>
 
-            <div style={{marginTop: 24, paddingBottom: 48, background: '#1f1f1f'}}>
-                <div style={{paddingTop: 12, textAlign: 'center'}}>
-                    <Title level={2}>
-                        THEM BUN LONG QUA
-                    </Title>
-                </div>
-                <Row justify={'center'} className={'banner-title'}>
-                    <Col xs={24} sm={24} md={24} lg={24} xl={12}>
-                        <Row>
-                            <Col xs={24} md={12}>
-                                <Card style={{margin: 4}}>
-                                    7749 User
-                                </Card>
-                            </Col>
+          <Row justify="center" className="banner-title">
+    <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+    <Row justify="center" align="middle">
+        <Col xs={12} md={6}>
+            <Card style={{ margin: 4, textAlign: "center", background: "rgba(10, 10, 12, 0.3)"}}>7749 User</Card>
+              </Col>
+        <Col xs={12} md={6}>
+            <Card style={{ margin: 4, textAlign: "center", background: "rgba(10, 10, 12, 0.3)"}}>9981 Accounts</Card>
+              </Col>
+             </Row>
+           </Col>
+         </Row>
+        </div>
+      </Content>
 
-                            <Col xs={24} md={12}>
-                                <Card style={{margin: 4}}>
-                                    9981 Accounts
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </div>
-
-            <Footer style={{color: "white", textAlign: 'center'}}>
-                <Title level={2}>
-                    THEM BUN LONG QUA
-                </Title>
-            </Footer>
-
-        </Content>
-
-    );
+      <Footer style={{ color: 'white', textAlign: 'center', background: 'rgba(10, 10, 12, 0.2)', padding: '24px 50px' }}>
+        <Title level={2}>THEM BUN LONG QUA</Title>
+      </Footer>
+    </Layout>
+  );
 };
 
 export default Landing;
